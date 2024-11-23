93809c27a335be52a9d6ba2eb47d18997bc54f15
matve <matvey.karasev.00@gmail.com>
2024-11-23 12:51:45 +0300

    task2 was completed

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,19 @@
+import java.lang.Math;
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+        int[] arr = new int[10];
+        int av = 0, counter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 40+ (int) (Math.random() * 60);
+            System.out.println(arr[i]);
+            av+=arr[i];
+            if (arr[i]>=60 && arr[i]<=80) counter++;
+        }
+
+        System.out.println();
+        System.out.println(counter);
+        System.out.println((int)(av/arr.length));
     }
 }
