7a745a5c173fe6636df3a638f6d5ac1cd79beae1
matve <matvey.karasev.00@gmail.com>
2024-11-23 12:34:35 +0300

    task1 was completed

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -2,14 +2,38 @@ public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
-
-        //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i*i;
+        }
+        System.out.println();
+        System.out.println();//#2 Распечатать все значения массива начиная с 0 до последнего индексов.
         //todo
-
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+        System.out.println();
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
         //todo
-
+        for (int i = arr.length-1; i>=0; i--) {
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+        System.out.println();
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
+        for (int i = 0; i < 5; i++) {
+            int buf = arr[i];
+            arr[i]=arr[arr.length-1-i];
+            arr[arr.length-1-i]=buf;
+
+        }
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
